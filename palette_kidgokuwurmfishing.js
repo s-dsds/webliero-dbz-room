2bf5fc02e1742905f416b5d7de11264dcf92c210
Quentin Morlais <quentin@abtasty.com>
2020-10-19 12:29:39 +0200

    add kid goku wurm fishing map

---
+++ palette_kidgokuwurmfishing.js
@@ -0,0 +1,262 @@
+var pt_kidgokuf = `GIMP Palette
+Name: wl_gokuk_full_unique
+Columns: 16
+#
+  0   0   0	    undefined
+108  56   0	    dirt
+108  80   0	    dirt
+164 148 128	    undefined
+  0 144   0	    undefined
+ 61 173  61	    undefined
+252  84  84	    undefined
+168 168 168	    undefined
+ 85  85  85	    undefined
+ 84  84 252	    undefined
+ 84 216  84	    undefined
+ 84 252 252	    undefined
+ 13  19  73	#1 dirt
+ 14  26 104	#2 dirt
+ 14  26 136	#3 dirt
+ 17  27 121	#4 dirt
+  8  39 163	#5 dirt
+  3  46 183	#6 dirt
+ 33  45 157	#8 dirt
+  4  21   7	       #2 rock
+ 20  34  15	       #3 rock
+ 51  36  87	       #4 rock
+ 34  49  25	       #5 rock
+109 109 109	    rock
+116 116 116	    rock
+125 125 125	    rock
+ 38  42  28     	#0 rock
+ 95  94  76     	#1 rock
+140 129 112     	#2 rock
+168 162 137     	#3 rock
+ 56  56 136	    worm
+ 81  81 193	    worm
+105 105 249	    worm
+145 145 245	    worm
+185 185 245	    worm
+110 110 110	    worm
+145 145 145	    worm
+181 181 181	    worm
+217 217 217	    worm
+ 32  96  32	    undefined
+ 45 133  45	    undefined
+ 62 174  62	    undefined
+113 189 113	    undefined
+165 213 165	    undefined
+111 111 111	    rock
+146 146 146	    undefined
+182 182 182	    undefined
+218 218 218	    undefined
+168 168 248	    undefined
+208 208 244	    undefined
+252 252 244	    undefined
+ 60  80   0	    undefined
+ 88 112   0	    undefined
+116 144   0	    undefined
+148 176   0	    undefined
+  6  52 203			#9 dirt
+ 53  55  68			#11 dirt
+ 52  58  83			#13 dirt
+ 45  89 123			#16 dirt
+173  47  10      	#9 rock
+255 255 255	        #15 rock
+214 177 163	        #16 rock
+200 100   0	    undefined
+160  80   0	    undefined
+ 72  72  72	    undefined
+108 108 108	    undefined
+147 147 147	    undefined
+180 180 180	    undefined
+216 216 216	    undefined
+253 253 253	    undefined
+196 196 196	    undefined
+144 144 144	    undefined
+152  60   0	    undefined
+180 100   0	    undefined
+208 140   0	    undefined
+236 180   0	    undefined
+168  84   0	    undefined
+161  22  24		#10 bg dirt
+189   1   1	    dirt
+165   1   1	    dirt
+200   0   0	    undefined
+172   0   0	    undefined
+218   2   2	    dirt
+190   2   2	    dirt
+166   2   2	    dirt
+228   0   9	          #6 rock
+ 13   5  24			#1   rock
+ 8   39  163		#11  rock           ###
+ 75 129 170				#26 dirt
+ 32 106 162			#19 dirt
+ 66  68  81			#14 dirt
+ 77  58 139			#8 rock
+106  84 169			#13  rock
+ 89  70 150			#10 rock
+ 109  93  79	#20 dirt
+124  94  82	    #21 dirt
+108 108  96	   #22  dirt
+133 107  79  	#24 dirt
+108 113  86	   #23 dirt
+ 81  82  79	  #17 dirt
+ 92  94  82  	#18 dirt
+120 121  79	  #25 dirt
+131 130 117	    #27 dirt
+131 134  80	   #28 dirt
+253   1   1	    undefined
+248  24   4	    undefined
+248  52   8	    undefined
+248  80  16	    undefined
+248 108  20	    undefined
+248 136  24	    undefined
+248 164  32	    undefined
+248 192  36	    undefined
+248 220  40	    undefined
+245 233  61	    undefined
+244 244  80	    undefined
+244 244 112	    undefined
+244 244 148	    undefined
+240 240 180	    undefined
+240 240 212	    undefined
+240 240 248	    undefined
+ 24  52  86			#7 dirt
+ 40  60  77			#12 dirt
+ 66  81  81			#15 dirt
+ 48  74  35	        #7  rock
+ 67 101  46	        #12 rock
+17  27   121	        #14 rock       ###
+248  60  60	    undefined
+244 124 124	    undefined
+244 188 188	    undefined
+104 104 248	    led
+177 199 133     	#8 bg led
+184 184 244	    led
+144 144 244	    undefined
+ 65 177  65	    undefined
+116 192 116	    undefined
+164 212 164	    undefined
+112 188 112	    undefined
+148 136   0	    undefined
+136 116   0	    undefined
+124  96   0	    undefined
+112  76   0	    undefined
+100  56   0	    undefined
+ 89  41   1	    undefined
+104 104 136	    undefined
+144 144 192	    undefined
+188 188 248	    undefined
+200 200 244	    undefined
+220 220 244	    undefined
+ 40 112  40	    undefined
+ 44 132  44	    undefined
+ 52 152  52	    undefined
+ 60 172  60	    undefined
+252 200 200	    led
+245 165 165	        led
+248  92  92	        led
+245  77  77	        led
+244  60  60	        led
+244  76  76	        led
+244  92  92	        led
+244 164 164	        led
+ 88  90  48	           #0  bg    see shadow
+ 83 101  60	         #1 bg    see shadow
+ 85 106  47         	#2 bg    see shadow
+105 109  65      	#3	bg    see shadow
+104 120  75			#4 bg
+119 138  81			#5 bg
+137 158  95			#6 bg
+154 175 111			#7 bg
+252 252 252	        led
+221 221 221	        led
+189 189 189	        led
+158 158 158	        led
+124 124 124	    undefined
+156 156 156	    undefined
+188 188 188	    undefined
+220 220 220	    undefined
+162 130  84   		#29 dirt
+137 138 108 		#30 dirt
+166 132 107 		#31 dirt
+178 151 107 		#32 dirt
+188 150 123 		#33 dirt
+  0   0   0	    undefined
+ 40  36   8	    undefined
+ 80  76  20	    undefined
+120 116  28	    undefined
+160 152  40	    undefined
+200 192  48	    undefined
+244 232  60	    undefined
+  0   0   0	    undefined
+  0   0   0	    undefined
+  0   0   0	    undefined
+  0   0   0	    undefined
+  0   0   0	    undefined
+  0   0   0	    undefined
+  0   0   0	    undefined
+  0   0   0	    undefined
+  0   0   0	    undefined
+  0   0   0	    undefined
+  0   0   0	    undefined
+  0   0   0	    undefined
+  0   0   0	    undefined
+  0   0   0	    undefined
+  0   0   0	    undefined
+  0   0   0	    undefined
+  0   0   0	    undefined
+  0   0   0	    undefined
+  0   0   0	    undefined
+  0   0   0	    undefined
+  0   0   0	    undefined
+  0   0   0	    undefined
+  0   0   0	    undefined
+  0   0   0	    undefined
+  0   0   0	    undefined
+  0   0   0	    undefined
+  0   0   0	    undefined
+  0   0   0	    undefined
+  0   0   0	    undefined
+  0   0   0	    undefined
+  0   0   0	    undefined
+  0   0   0	    undefined
+  0   0   0	    undefined
+  0   0   0	    undefined
+ 22  77  26	#0 grass  undef
+ 11 105  21	#1 grass   undef
+163  96  46	#0 skin  undef
+201 122  60	#1 skin  undef
+204 131  73	#2 skin  undef
+209 136  84	#3 skin  undef
+205 141 110	#4 skin  undef
+191 151 130	#5 skin  undef
+219 160 119	#6 skin  undef
+217 164 121	#7 skin  undef
+217 173 139	#8 skin  undef
+225 179 139	#9 skin  undef
+254   2   2	    undefined
+252  36   0	    undefined
+252  72   0	    undefined
+252 108   0	    undefined
+252 144   0	    undefined
+252 180   0	    undefined
+252 216   0	    undefined
+252 252   0	    undefined
+168 240   0	    undefined
+ 84 232   0	    undefined
+  0 224   0	    undefined
+252   0   0	    undefined
+232   4  20	    undefined
+216  12  44	    undefined
+196  20  68	    undefined
+180  24  88	    undefined
+160  32 112	    undefined
+144  40 136	    undefined
+124  44 156	    undefined
+108  52 180	    undefined
+ 88  60 204	    undefined
+ 72  68 228	    undefined`;
+
+addPalette('kidgokuf', parseGimpPalette(pt_kidgokuf)); pt_kidgokuf=null;
