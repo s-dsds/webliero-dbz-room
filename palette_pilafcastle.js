0ffbe984e85c1cb00bc57c608731fc38b6721647
Quentin Morlais <quentin@abtasty.com>
2020-10-21 13:58:11 +0200

    adds map pilaf castle

---
+++ palette_pilafcastle.js
@@ -0,0 +1,263 @@
+var pt_pc = `GIMP Palette
+Name: wl_pc_full_unique
+Columns: 16
+#
+  0   0   0	    undefined
+108  56   0	    dirt
+108  80   0	    dirt
+164 148 128	    undefined
+  0 144   0	    undefined
+ 61 173  61	    undefined
+252  84  84	    undefined
+168 168 168	    undefined
+ 85  85  85	    undefined
+ 84  84 252	    undefined
+ 84 216  84	    undefined
+ 84 252 252	    undefined
+146 129 174	#0 dirt
+144 135 177	#1 dirt
+150 140 183	#2 dirt
+156 147 190	#3 dirt
+163 153 196	#4 dirt
+168 158 201	#5 dirt
+165 163 204	#6 dirt
+ 49  42  27	#0 rock gm
+106 108 105	#5 rock g
+145 148 145	#13 rock g
+155 158 154	#16 rock g
+182 168 138	#18 rock g
+102  79  47	#1 rock m
+107  81  42	#2 rock m
+116  91  53	#3 rock m
+121  93  47	#4 rock m
+134 106  63	#6 rock m
+152 123  82	#8 rock m
+ 56  56 136	    worm
+ 81  81 193	    worm
+105 105 249	    worm
+145 145 245	    worm
+185 185 245	    worm
+110 110 110	    worm
+145 145 145	    worm
+181 181 181	    worm
+217 217 217	    worm
+ 32  96  32	    undefined
+ 45 133  45	    undefined
+ 62 174  62	    undefined
+113 189 113	    undefined
+165 213 165	    undefined
+111 111 111	    rock
+146 146 146	    undefined
+182 182 182	    undefined
+218 218 218	    undefined
+168 168 248	    undefined
+208 208 244	    undefined
+252 252 244	    undefined
+ 60  80   0	    undefined
+ 88 112   0	    undefined
+116 144   0	    undefined
+148 176   0	    undefined
+120  72  52	    dirt
+157 121  89	    dirt
+197 169 125	    dirt
+237 217 161	    dirt
+177 138  80	#11 rock m
+179 147  99	#14 rock m
+198 160  80	#17 rock m
+200 100   0	    undefined
+160  80   0	    undefined
+ 72  72  72	    undefined
+108 108 108	    undefined
+147 147 147	    undefined
+180 180 180	    undefined
+216 216 216	    undefined
+253 253 253	    undefined
+196 196 196	    undefined
+144 144 144	    undefined
+152  60   0	    undefined
+180 100   0	    undefined
+208 140   0	    undefined
+236 180   0	    undefined
+168  84   0	    undefined
+217   1   1	    dirt
+189   1   1	    dirt
+165   1   1	    dirt
+200   0   0	    undefined
+172   0   0	    undefined
+171 167 210	#7 dirt
+176 173 215	#8 dirt
+182 179 222	#9 dirt
+177  99 117	#7 rock r
+207 126 144	#12 rock r
+215 134 152	#15 rock r
+ 82  82 194	    dirt
+106 106 250	    dirt
+146 146 246	    dirt
+155 123  75	#9 rock m
+169 136  88	#10 rock m
+233 171 187	#19 rock r
+149 137   1	    dirt
+136 124   0	    dirt
+124 112   0	    dirt
+116 100   0	    dirt
+132  92  40	    dirt
+160 132  72	    dirt
+188 176 104	    dirt
+216 220 136	    dirt
+248 248 188	    dirt
+244 244 252	    dirt
+253   1   1	    undefined
+248  24   4	    undefined
+248  52   8	    undefined
+248  80  16	    undefined
+248 108  20	    undefined
+248 136  24	    undefined
+248 164  32	    undefined
+248 192  36	    undefined
+248 220  40	    undefined
+245 233  61	    undefined
+244 244  80	    undefined
+244 244 112	    undefined
+244 244 148	    undefined
+240 240 180	    undefined
+240 240 212	    undefined
+240 240 248	    undefined
+ 46 134  46	    dirt
+ 63 175  63	    dirt
+114 190 114	    dirt
+ 47 135  47	    rock
+ 64 176  64	    rock
+115 191 115	    rock
+248  60  60	    undefined
+244 124 124	    undefined
+244 188 188	    undefined
+104 104 248	    led
+148 148 248	    bg led
+184 184 244	    led
+144 144 244	    undefined
+ 65 177  65	    undefined
+116 192 116	    undefined
+164 212 164	    undefined
+112 188 112	    undefined
+148 136   0	    undefined
+136 116   0	    undefined
+124  96   0	    undefined
+112  76   0	    undefined
+100  56   0	    undefined
+ 89  41   1	    undefined
+104 104 136	    undefined
+144 144 192	    undefined
+188 188 248	    undefined
+200 200 244	    undefined
+220 220 244	    undefined
+ 40 112  40	    undefined
+ 44 132  44	    undefined
+ 52 152  52	    undefined
+ 60 172  60	    undefined
+252 200 200	    led
+245 165 165	        led
+248  92  92	        led
+245  77  77	        led
+244  60  60	        led
+244  76  76	        led
+244  92  92	        led
+244 164 164	        led
+138 113  50	#0 bg ss
+139 123  77	#1 bg ss
+145 131  98	#2 bg ss
+153 135  83	#3 bg ss
+165 136  62	#4 bg
+171 151  93	#5 bg
+177 161 120	#6 bg
+180 167 137	#7 bg
+252 252 252	        led
+221 221 221	        led
+189 189 189	        led
+158 158 158	        led
+124 124 124	    undefined
+156 156 156	    undefined
+188 188 188	    undefined
+220 220 220	    undefined
+108  76  44	    dirt
+124  84  48	    dirt
+140  96  56	    dirt
+156 108  64	    dirt
+172 120  72	    dirt
+  0   0   0	    undefined
+ 40  36   8	    undefined
+ 80  76  20	    undefined
+120 116  28	    undefined
+160 152  40	    undefined
+200 192  48	    undefined
+244 232  60	    undefined
+  0   0   0	    undefined
+  0   0   0	    undefined
+  0   0   0	    undefined
+  0   0   0	    undefined
+  0   0   0	    undefined
+  0   0   0	    undefined
+  0   0   0	    undefined
+  0   0   0	    undefined
+  0   0   0	    undefined
+  0   0   0	    undefined
+  0   0   0	    undefined
+  0   0   0	    undefined
+  0   0   0	    undefined
+  0   0   0	    undefined
+  0   0   0	    undefined
+  0   0   0	    undefined
+  0   0   0	    undefined
+  0   0   0	    undefined
+  0   0   0	    undefined
+  0   0   0	    undefined
+  146 129 174	#0 undef inv 	#0 dirt
+  144 135 177	#1 undef inv 	#1 dirt
+  150 140 183	#2 undef inv 	#2 dirt
+  156 147 190	#3 undef inv 	#3 dirt
+  163 153 196	#4 undef inv 	#4 dirt
+  168 158 201	#5 undef inv 	#5 dirt
+  165 163 204	#6 undef inv 	#6 dirt
+  171 167 210	#7 undef inv 	#7 dirt
+  176 173 215	#8 undef inv 	#8 dirt
+  182 179 222	#9 undef inv 	#9 dirt
+  0   0   0	    undefined
+ 79  11   8	#0 undef
+101   9   8	#1 undef
+136   7   6	#2 undef
+155   4   2	#3 undef
+169   0   4	#4 undef
+179   0   0	#5 undef
+191   0   0	#6 undef
+216   0   0	#7 undef
+101  86 127	#8 undef
+127 118 160	#9 undef
+146 129 174	#10 undef
+178 180 178	#11 undef
+197 199 196	#12 undef
+210 213 209	#13 undef
+233 235 232	#14 undef
+254   2   2	    undefined
+252  36   0	    undefined
+252  72   0	    undefined
+252 108   0	    undefined
+252 144   0	    undefined
+252 180   0	    undefined
+252 216   0	    undefined
+252 252   0	    undefined
+168 240   0	    undefined
+ 84 232   0	    undefined
+  0 224   0	    undefined
+252   0   0	    undefined
+232   4  20	    undefined
+216  12  44	    undefined
+196  20  68	    undefined
+180  24  88	    undefined
+160  32 112	    undefined
+144  40 136	    undefined
+124  44 156	    undefined
+108  52 180	    undefined
+ 88  60 204	    undefined
+ 72  68 228	    undefined
+`;
+
+addPalette('pilafcastle', parseGimpPalette(pt_pc)); pt_pc=null;
