7ef0efe65e6bde3e78ba712780c4b093b7293edf
Quentin Morlais <quentin@abtasty.com>
2020-10-26 21:25:32 +0100

    map kamehouse

---
+++ palette_kamehouse_int.js
@@ -0,0 +1,263 @@
+var pt_kih = `GIMP Palette
+Name: wl_khi_full
+Columns: 16
+#
+  0   0   0	    undefined
+ 96  69  68	#1 background
+154 100  45	#4 background
+  4  41   8	#1 undef
+ 43  30  20	#2 undef
+  5  53   6	#3 undef
+ 60  37  12	#4 undef
+  1  60   1	#5 undef
+  1  63 106	#6 undef
+ 86  45  13	#7 undef
+ 44  55  51	#8 undef
+ 62  58   9	#9 undef
+120  64   8	    dirt
+128  68   8	    dirt
+136  72  12	    dirt
+144  80  16	    dirt
+152  84  20	    dirt
+160  88  24	    dirt
+172  96  28	    dirt
+ 26  26  26	#0 rocks
+ 51  26  15	#1 rocks
+ 63  42  14	#2 rocks
+ 33  66   7	#3 rocks
+ 97  53   5	#4 rocks
+ 97  71  69	#5 rocks
+ 80  81  74	#6 rocks
+ 51 111  36	#7 rocks
+127  88  58	#8 rocks
+ 81  98 118	#9 rocks
+139  94  26	#10 rocks
+ 56  56 136	    worm
+ 81  81 193	    worm
+105 105 249	    worm
+145 145 245	    worm
+185 185 245	    worm
+110 110 110	    worm
+145 145 145	    worm
+181 181 181	    worm
+217 217 217	    worm
+  6  77   6	#10 undef
+ 58  67  55	#11 undef
+ 44  77  31	#12 undef
+  7  91   7	#13 undef
+ 34  79  53	#14 undef
+157 107 120	#11 rocks
+176   9   9	#7 red
+197   5  10	#8 red
+228   0   6	#9 red
+254   0   0	#10 red
+221  10  12	#11 red
+212  16   5	#12 red
+212  16  16	#13 red
+ 24  76 138	#15 undef
+100  63  38	#16 undef
+ 55  76  60	#17 undef
+120  72  52	    dirt
+157 121  89	    dirt
+197 169 125	    dirt
+237 217 161	    dirt
+176 116  92	#12 rocks
+165 127  27	#13 rocks
+148 151 151	#14 rocks
+32  82 145	#18 undef
+ 60  80  76	#19 undef
+ 24  85 153	#20 undef
+ 53  86  38	#21 undef
+ 53  87  32	#22 undef
+ 39  85 113	#23 undef
+ 22  89 138	#24 undef
+ 24  92 135	#25 undef
+ 16 106  13	#26 undef
+116  75  22	#27 undef
+101  79  61	#28 undef
+ 76  87  72	#29 undef
+ 83  85  82	#30 undef
+ 30  97 146	#31 undef
+ 30 101 143	#32 undef
+217   1   1	    dirt
+189   1   1	    dirt
+165   1   1	    dirt
+ 58 101  42	#33 undef
+ 50 100 128	#34 undef
+218   2   2	    dirt
+190   2   2	    dirt
+166   2   2	    dirt
+ 92   6   2	#0 red rock
+152   5   9	#1 red rock
+143   8   7	#2 red rock
+ 82  82 194	    dirt
+106 106 250	    dirt
+146 146 246	    dirt
+157 151 126	#15 rocks
+255 169 144	#16 rocks
+206 205 203	#17 rocks
+149 137   1	    dirt
+136 124   0	    dirt
+124 112   0	    dirt
+116 100   0	    dirt
+132  92  40	    dirt
+160 132  72	    dirt
+188 176 104	    dirt
+216 220 136	    dirt
+248 248 188	    dirt
+244 244 252	    dirt
+ 44 100 168	#35 undef
+ 40 105 137	#36 undef
+ 31 107 170	#37 undef
+155  75 113	#38 undef
+ 33 110 151	#39 undef
+ 54 107 150	#40 undef
+ 40 110 167	#41 undef
+ 92 103  85	#42 undef
+ 70 114  48	#43 undef
+ 40 113 176	#44 undef
+ 63 112 104	#45 undef
+ 33 118 174	#46 undef
+ 62 117  80	#47 undef
+ 49 115 172	#48 undef
+ 77 110 132	#49 undef
+ 88 110 103	#50 undef
+ 46 134  46	    dirt
+ 63 175  63	    dirt
+114 190 114	    dirt
+253 216   0	#18 rocks
+255 255 255	#19 rocks
+115 191 115	    rock
+ 47 118 160	#51 undef
+ 65 112 176	#52 undef
+150  98  51	#53 undef
+104 104 248	    led
+ 43  73 118	#0 background   !!led
+184 184 244	    led
+115 106  85	#54 undef
+ 29 127 166	#55 undef
+ 38 124 173	#56 undef
+ 69 116 157	#57 undef
+ 37 124 180	#58 undef
+103 115  94	#59 undef
+ 58 122 182	#60 undef
+ 67 123 175	#61 undef
+ 35 135 171	#62 undef
+ 46 132 189	#63 undef
+ 30 138 180	#64 undef
+ 36 137 185	#65 undef
+ 46 135 178	#66 undef
+ 53 133 183	#67 undef
+109 126  71	#68 undef
+ 39 140 169	#69 undef
+ 55 135 173	#70 undef
+ 64 131 191	#71 undef
+ 78 128 185	#72 undef
+ 66 134 178	#73 undef
+252 200 200	    led
+245 165 165	        led
+248  92  92	        led
+245  77  77	        led
+244  60  60	        led
+244  76  76	        led
+244  92  92	        led
+244 164 164	        led
+134  91 102	#2 background
+138 100  75	#3 background
+179 120 136	#5 background
+196 137 152	#6 background
+150 152 149	#7 background
+189 158 144	#8 background
+195 197 194	#9 background
+193 207 207	#10 background
+252 252 252	        led
+221 221 221	        led
+189 189 189	        led
+158 158 158	        led
+ 27 147 176	#74 undef
+ 34 145 180	#75 undef
+137 122  85	#76 undef
+ 99 130 138	#77 undef
+108  76  44	    dirt
+124  84  48	    dirt
+140  96  56	    dirt
+156 108  64	    dirt
+172 120  72	    dirt
+ 93 132 139	#78 undef
+ 41 145 187	#79 undef
+ 50 143 179	#80 undef
+ 80 138 142	#81 undef
+ 88 136 141	#82 undef
+ 35 150 172	#83 undef
+ 61 143 174	#84 undef
+ 64 142 180	#85 undef
+ 51 147 176	#86 undef
+ 34 153 169	#87 undef
+ 67 141 192	#88 undef
+ 55 145 191	#89 undef
+120 132 109	#90 undef
+ 90 139 144	#91 undef
+ 85 142 146	#92 undef
+ 54 150 168	#93 undef
+ 37 156 166	#94 undef
+ 78 145 148	#95 undef
+ 99 139 146	#96 undef
+ 96 141 141	#97 undef
+175 119 136	#98 undef
+103 142 133	#99 undef
+ 66 150 188	#100 undef
+ 96 144 149	#101 undef
+ 80 148 193	#102 undef
+ 75 153 154	#103 undef
+128 140 136	#104 undef
+ 85 152 154	#105 undef
+ 93 150 154	#106 undef
+ 74 156 175	#107 undef
+108 148 154	#108 undef
+102 150 155	#109 undef
+104 148 177	#110 undef
+105 151 150	#111 undef
+110 151 139	#112 undef
+109 151 145	#113 undef
+ 90 161 163	#114 undef
+210 131  88	#115 undef
+110 159 160	#116 undef
+118 157 161	#117 undef
+149 151 130	#118 undef
+171 154 112	#119 undef
+109 170 171	#120 undef
+127 165 168	#121 undef
+122 169 170	#122 undef
+168 163 109	#123 undef
+138 173 176	#124 undef
+122 178 178	#125 undef
+164 167 164	#126 undef
+135 182 182	#127 undef
+190 171 125	#128 undef
+147 181 183	#129 undef
+197 174 122	#130 undef
+160 183 191	#131 undef
+156 188 191	#132 undef
+202 180 119	#133 undef
+167 186 185	#134 undef
+167 197 200	#135 undef
+175 196 196	#136 undef
+175 206 209	#137 undef
+191 202 202	#138 undef
+183 205 208	#139 undef
+191 214 218	#140 undef
+200 212 213	#141 undef
+207 220 221	#142 undef
+214 219 221	#143 undef
+214 227 228	#144 undef
+223 228 231	#145 undef
+229 236 238	#146 undef
+238 243 246	#147 undef
+242 245 242	#148 undef
+255 255 255	#149 undef
+108  52 180	    undefined
+ 88  60 204	    undefined
+ 72  68 228	    undefined
+`;
+
+addPalette('khi', parseGimpPalette(pt_kih)); pt_kih=null;
