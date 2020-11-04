4d0f2927638b42bd5ba4bc582203162bbd35df10
Quentin Morlais <quentin@abtasty.com>
2020-11-04 13:42:29 +0100

    adds 104

---
+++ palette_104.js
@@ -0,0 +1,263 @@
+var pt_104 = `GIMP Palette
+Name: wl_kang_full_unique
+Columns: 16
+#
+  0   0   0	    undefined
+ 11  73  11	#0 bg-dirt green
+ 14 102  14	#1 bg-dirt green
+164 148 128	    undefined
+  0 144   0	    undefined
+ 61 173  61	    undefined
+252  84  84	    undefined
+168 168 168	    undefined
+ 85  85  85	    undefined
+ 84  84 252	    undefined
+ 84 216  84	    undefined
+ 84 252 252	    undefined
+120  64   8	    dirt
+128  68   8	    dirt
+136  72  12	    dirt
+144  80  16	    dirt
+152  84  20	    dirt
+160  88  24	    dirt
+172  96  28	    dirt
+ 45  52  52	#11 rock_undef grey dark
+ 69  71  88	#19 rock_undef grey 
+ 72  80  88	#22 ROCK grey
+ 98 101 118	#33 rock_undef grey
+112 109  93	#38 rock_undef grey
+101 113 127	#41 ROCK grey
+ 81  38  31	#9 ROCK brown
+131  63   1	#20 ROCK brown
+182  85   0	#34 ROCK orange brown
+168  97  25	#37 ROCK orange brown
+252 140  23	#63 ROCK orange
+ 56  56 136	    worm
+ 81  81 193	    worm
+105 105 249	    worm
+145 145 245	    worm
+185 185 245	    worm
+110 110 110	    worm
+145 145 145	    worm
+181 181 181	    worm
+217 217 217	    worm
+ 25  78  36	#13 rock_undef green
+ 37  77  40	#15 rock_undef green
+ 11  88  18	#16 rock_undef green
+ 33 166  33	#47 rock_undef green
+120 127 134	#44 rock_undef grey
+111 111 111	    rock
+146 146 146	    undefined
+182 182 182	    undefined
+218 218 218	    undefined
+168 168 248	    undefined
+208 208 244	    undefined
+252 252 244	    undefined
+ 60  80   0	    undefined
+ 88 112   0	    undefined
+116 144   0	    undefined
+148 176   0	    undefined
+120  72  52	    dirt
+157 121  89	    dirt
+197 169 125	    dirt
+237 217 161	    dirt
+100  91  77	#28 ROCK
+149 135 107	#54 ROCK
+210 192 153	#75 ROCK
+200 100   0	    undefined
+160  80   0	    undefined
+ 72  72  72	    undefined
+108 108 108	    undefined
+147 147 147	    undefined
+180 180 180	    undefined
+226 221 221	#78 rock_undef white-grey
+255 255 255	#79 rock_undef white
+196 196 196	    undefined
+144 144 144	    undefined
+152  60   0	    undefined
+142  76  48	#26 rock_undef brown
+208 127  65	#57 rock_undef orange
+202 149   3	#61 rock_undef orange light
+133  70  53	#24 rock_undef brown
+217   1   1	    dirt
+189   1   1	    dirt
+165   1   1	    dirt
+200   0   0	    undefined
+172   0   0	    undefined
+218   2   2	    dirt
+190   2   2	    dirt
+166   2   2	    dirt
+216   0   0	    rock
+129   7   4	#7 ROCK red
+165   0   3	#8 ROCK red
+ 82  82 194	    dirt
+106 106 250	    dirt
+146 146 246	    dirt
+  8  29 100	#4 ROCK blue
+  5  27 150	#5 ROCK  blue
+ 99 153 174	#58 ROCK blue light grey 
+149 137   1	    dirt
+136 124   0	    dirt
+124 112   0	    dirt
+116 100   0	    dirt
+132  92  40	    dirt
+160 132  72	    dirt
+188 176 104	    dirt
+216 220 136	    dirt
+207 178 189	#2 dirt pink light
+233 203 211	#3 dirt pink light
+253   1   1	    undefined
+248  24   4	    undefined
+248  52   8	    undefined
+200 115  30	#48 rock_undef orange
+214 116  13	#51 rock_undef orange
+239 134  28	#60 rock_undef orange
+248 164  32	    undefined
+248 192  36	    undefined
+248 220  40	    undefined
+245 233  61	    undefined
+244 244  80	    undefined
+244 244 112	    undefined
+244 244 148	    undefined
+240 240 180	    undefined
+240 240 212	    undefined
+240 240 248	    undefined
+ 46 134  46	    dirt
+196  65 131	#0 dirt pink
+194  91 144	#1 dirt pink
+  0 119  26	#25 ROCK green
+ 45 117  61	#30 ROCK green
+  0 180  27	#50 ROCK green
+248  60  60	    undefined
+244 124 124	    undefined
+244 188 188	    undefined
+104 104 248	    led
+148 148 248	    bg led
+184 184 244	    led
+144 144 244	    undefined
+ 65 177  65	    undefined
+116 192 116	    undefined
+164 212 164	    undefined
+112 188 112	    undefined
+148 136   0	    undefined
+136 116   0	    undefined
+124  96   0	    undefined
+112  76   0	    undefined
+100  56   0	    undefined
+ 89  41   1	    undefined
+104 104 136	    undefined
+ 52 106 165	#32 rock_undef  blue
+ 95 134 181	#46 rock_undef blue light
+158 192 220	#72 rock_undef  blue light
+220 220 244	    undefined
+ 49 162  36	#49 rock_undef green
+122 190  68	#67 rock_undef green
+ 50 152  78	Sans titre green
+ 60 172  60	    undefined
+252 200 200	    led
+245 165 165	        led
+248  92  92	        led
+245  77  77	        led
+244  60  60	        led
+244  76  76	        led
+244  92  92	        led
+244 164 164	        led
+ 52 108 165	#0 bg sky blue
+ 95 134 182	#1 bg sky blue
+169 189 222	#2 bg sky blue
+178 157  92	#4 bg-ground light brown
+  6  73  25	#0 bg-ground green
+ 37 132  60	#1 bg-ground green
+ 50 152  77	#2 bg-ground green
+151 127  65	#3 bg-ground light brown
+252 252 252	        led
+221 221 221	        led
+189 189 189	        led
+158 158 158	        led
+124 124 124	    undefined
+156 156 156	    undefined
+188 188 188	    undefined
+220 220 220	    undefined
+108  76  44	    dirt
+124  84  48	    dirt
+140  96  56	    dirt
+156 108  64	    dirt
+172 120  72	    dirt
+  0   0   0	    undefined
+ 40  36   8	    undefined
+ 80  76  20	    undefined
+120 116  28	    undefined
+160 152  40	    undefined
+200 192  48	    undefined
+244 232  60	    undefined
+  0   0   0	    undefined
+ 19   8  12	#1 rock_undef dark
+ 17  21  23	#2 rock_undef dark
+ 26  27  25	#3 rock_undef dark
+ 55  24  13	#6 rock_undef red dark
+ 92  41   2	#10 rock_undef brown
+ 76  58  24	#12 rock_undef brown
+105  53  40	#14 rock_undef brown
+117  73  16	#23 rock_undef brown
+232 134  91	#62 rock_undef pinky-orange light
+239 151  90	#66 rock_undef pinky-orange light
+209 117  81	#52 rock_undef pink
+187 124  84	#53 rock_undef pink grey
+213 150 124	#64 rock_undef pink greyish light
+231 168 150	#71 rock_undef pink light
+207 167 133	#69 rock_undef pink light greyish
+ 73  73  33	#17 rock_undef
+ 86  71  23	#18 rock_undef 
+ 84  76  66	#21 rock_undef 
+ 93  95  32	#27 rock_undef
+116  95  34	#29 rock_undef
+110  97  69	#31 rock_undef
+112 106  36	#35 rock_undef
+127 105  44	#36 rock_undef
+120 109  87	#39 rock_undef
+174  94  67	#40 rock_undef
+142 118  58	#42 rock_undef
+175 134  94	#56 rock_undef
+135 123  97	#43 rock_undef
+131 132  57	#45 rock_undef greenish grayish
+143 138 113	#55 rock_undef
+162 148 119	#59 rock_undef
+177 162 127	#65 rock_undef
+177 170 141	#68 rock_undef
+191 176 140	#70 rock_undef
+199 191 157	#73 rock_undef
+202 195 137	#74 rock_undef
+184 219 179	#76 rock_undef green light
+  0   0   0	    undefined
+  0   0   0	    undefined
+  0   0   0	    undefined
+  0   0   0	    undefined
+  0   0   0	    undefined
+  0   0   0	    undefined
+  0   0   0	    undefined
+  0   0   0	    undefined
+254   2   2	    undefined
+252  36   0	    undefined
+252  72   0	    undefined
+252 108   0	    undefined
+252 144   0	    undefined
+252 180   0	    undefined
+253 222   0	#77 rock_undef yellow
+252 252   0	    undefined
+168 240   0	    undefined
+ 84 232   0	    undefined
+  0 224   0	    undefined
+252   0   0	    undefined
+232   4  20	    undefined
+216  12  44	    undefined
+196  20  68	    undefined
+180  24  88	    undefined
+160  32 112	    undefined
+144  40 136	    undefined
+124  44 156	    undefined
+108  52 180	    undefined
+ 88  60 204	    undefined
+ 72  68 228	    undefined
+`;
+
+addPalette('104', parseGimpPalette(pt_104)); pt_104=null;
